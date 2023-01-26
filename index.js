d374d53147d07fde8840e7c3be9ed41ec079678a
mekb the turtle <mekbgithub@icloud.com>
2023-01-27 00:11:40 +1100

    :tada: Initial commit

---
+++ index.js
@@ -0,0 +1,93 @@
+const memUsedColour = [ 245, 69, 198 ];
+const memAvailColour = [ 0, 0, 0 ];
+const lightIndexMem = 0;
+const lightReverseMem = true;
+
+const swapUsedColour = [ 41, 183, 249 ];
+const swapAvailColour = [ 0, 0, 0 ];
+const lightIndexSwap = 1;
+const lightReverseSwap = true;
+
+const sdkClient = {
+	host: "localhost",
+	port: 6742,
+	name: "RAM lights"
+};
+
+const { spawn } = require("child_process");
+const { OpenRGBClient, OpenRGBDevice } = require("openrgb");
+
+function colour(e) {
+	return e.map(f => ({ red: f[0], green: f[1], blue: f[2] }));
+}
+function lerpN(a, b, t) {
+	return (b-a)*t+a;
+}
+function lerp(a, b, t) {
+	return a.map((_, i) => lerpN(a[i], b[i], t));
+}
+function round(a) {
+	return a.map(i => Math.floor(i));
+}
+function percArr(perc, len) {
+	let arr = new Array(len).fill(0);
+	let n = perc * len;
+	let nF = Math.floor(n);
+	let nC = Math.ceil(n);
+	for (let i = 0; i < nF; ++i) {
+		arr[i] = 1;
+	}
+	if (nC > nF) {
+		arr[nF] = n - nF;
+	}
+	return arr;
+}
+function parseFree(str) {
+	let lines = str.split("\n");
+	let mem = lines[1].split(/ +/g);
+	let swap = lines[2].split(/ +/g);
+	let memUsed = +mem[2];
+	let memTotal = +mem[1];
+	let swapUsed = +swap[2];
+	let swapTotal = +swap[1];
+	let memPerc = memTotal > 0 ? memUsed / memTotal : 0;
+	let swapPerc = swapTotal > 0 ? swapUsed / swapTotal : 0;
+	return { mem: memPerc, swap: swapPerc };
+}
+function run(bin, cmdline) {
+	var proc = spawn(bin, cmdline);
+	let buffers = [];
+	proc.stdout.on("data", b => buffers.push(b));
+	proc.stdin.end();
+	return new Promise((resolve, reject) => {
+		proc.once("error", reject);
+		proc.once("exit", (c) => {
+			if (c > 0) reject("exited with code", c);
+			resolve(Buffer.concat(buffers));
+		});
+	});
+}
+async function setLight(client, index, reverse, perc, availColour, usedColour) {
+	if (index == null) return;
+	const device = await client.getDeviceController(index);
+	let colours = percArr(perc, device.colors.length).map(e => round(lerp(availColour, usedColour, e)));
+	if (reverse) colours = colours.reverse();
+	console.log(`Setting #${index} (${device.name}) to`, colours);
+	await client.updateLeds(index, colour(colours));
+}
+(async()=>{
+	let out = await run("/bin/free", [ ]);
+	let perc = parseFree(out.toString());
+	const client = new OpenRGBClient(sdkClient);
+	try {
+		await client.connect();
+	} catch (err) {
+		if (err.code == "ECONNREFUSED") {
+			console.log(`Try starting the SDK server with 'openrgb --server --server-port ${sdkClient.port}'`);
+		}
+		throw err;
+	}
+	await setLight(client, lightIndexMem, lightReverseMem, perc.mem, memAvailColour, memUsedColour);
+	await setLight(client, lightIndexSwap, lightReverseSwap, perc.swap, swapAvailColour, swapUsedColour);
+	await client.disconnect();
+})();
